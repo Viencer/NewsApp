903ad829ec6e8dfde083ce8ad2ea0660efa678b5
Viencer <yuriusis2001@gmail.com>
2020-03-14 20:51:58 +0200

    json parser with conversionService

--- src/main/java/com/spring/task/ntc_twoo/service/NewsService.java
+++ src/main/java/com/spring/task/ntc_twoo/service/NewsService.java
@@ -1,6 +1,5 @@
 package com.spring.task.ntc_twoo.service;
 
-import com.spring.task.ntc_twoo.model.Articles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.ConversionService;
@@ -18,21 +17,21 @@ public class NewsService implements NewsServiceIn {
     private ConversionService conversionService;
 
 //    @Autowired
-//    private JsonParser jsonParser;
+//    private JsonParser jsonParser; --- Вариант №2
 
     public NewsService(@Value("${apiKey}") String key) {
         this.urlSite = UriComponentsBuilder.fromHttpUrl("https://newsapi.org/v2/top-headlines").queryParam("apiKey", key);
     }
 
-    public List<Articles> categorySearch(String country, String category) {
+    public List categorySearch(String country, String category) {
         String url = urlSite.cloneBuilder().queryParam("country", country).queryParam("category", category).build().toString();
-//        return jsonParser.convert(url);
+//        return jsonParser.convert(url); --- Вариант №2
         return conversionService.convert(url, List.class);
     }
 
-    public List<Articles> countrySearch(String country) {
+    public List countrySearch(String country) {
         String url = urlSite.cloneBuilder().queryParam("country", country).build().toString();
-//        return jsonParser.convert(url);
+//        return jsonParser.convert(url); --- Вариант №2
         return conversionService.convert(url, List.class);
     }
 }
